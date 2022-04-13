0f0ad140199726e87297b7af1793d6fd7f288702
Arkin Modi <16737086+arkinmodi@users.noreply.github.com>
2022-04-13 18:09:28 -0400

    add example of how to add impl specific setup

--- src/test/java/com/arkinmodi/testinginterfaces/impl/ArrayListTest.java
+++ src/test/java/com/arkinmodi/testinginterfaces/impl/ArrayListTest.java
@@ -3,13 +3,33 @@ package com.arkinmodi.testinginterfaces.impl;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import com.arkinmodi.testinginterfaces.contract.ListTestContract;
 
 public class ArrayListTest implements ListTestContract {
 
     @Override
     public List<Integer> underTest() {
+
+        // Insert implementation specific setup here
+
         return new ArrayList<>();
+    }
+
+    @BeforeEach
+    public void setup() {
+        // Insert implementation specific setup here
+    }
+
+    @Override
+    @Test
+    public void testListSize() {
+
+        // Insert implementation specific setup here
+
+        ListTestContract.super.testListSize();
     }
 
 }
--- src/test/java/com/arkinmodi/testinginterfaces/impl/StackTest.java
+++ src/test/java/com/arkinmodi/testinginterfaces/impl/StackTest.java
@@ -3,13 +3,33 @@ package com.arkinmodi.testinginterfaces.impl;
 import java.util.List;
 import java.util.Stack;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import com.arkinmodi.testinginterfaces.contract.ListTestContract;
 
 public class StackTest implements ListTestContract {
 
     @Override
     public List<Integer> underTest() {
+
+        // Insert implementation specific setup here
+
         return new Stack<>();
+    }
+
+    @BeforeEach
+    public void setup() {
+        // Insert implementation specific setup here
+    }
+
+    @Override
+    @Test
+    public void testListSize() {
+
+        // Insert implementation specific setup here
+
+        ListTestContract.super.testListSize();
     }
 
 }
